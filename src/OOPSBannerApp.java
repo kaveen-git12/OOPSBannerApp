6c97a4da334e638511d2fd5eae9a5d7a064d467f
kaveen <kaveensmart28@gmail.com>
2026-03-02 15:37:41 +0530

    UC4

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -7,6 +7,6 @@ public class OOPSBannerApp {
         System.out.println(String.join("   ", "**    **", "**    **", "**     ", "    ** "));
         System.out.println(String.join("   ", "**    **", "**    **", "**     ", "    ** "));
         System.out.println(String.join("   ", " **  ** ", " **  ** ", "**     ", "**  ** "));
-           System.out.println(String.join("   ", "  ***   ", "  ***   ", "**     ", " **** "));
+        System.out.println(String.join("   ", "  ***   ", "  ***   ", "**     ", " **** "));
     }
 }
